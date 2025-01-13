dba0cc916a46ecab2785beb2d270ab32d8ccd7b7
kaznak <kaznak.at.work@ipebble.org>
2025-01-13 22:21:52 +0900

    setup tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
